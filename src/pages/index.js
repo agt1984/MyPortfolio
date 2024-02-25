a1c4946a2a120f46e6667136150c035569b3242c
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-02-25 18:38:58 +0100

    new files added

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+//creamos un index para poder exportar todas la paginas creadas al app.jsx
+import Home from './Home';
+import About from './About';
+import Projects from './Projects';
+import Contact from './Contact';
+
+export { Home, About, Projects, Contact };
