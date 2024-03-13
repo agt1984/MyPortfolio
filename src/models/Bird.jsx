2e4ee59ca391d66c8112d5922b68e187b9eeec4b
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-03-13 22:25:20 +0100

    new features added

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -1,20 +1,51 @@
 import { useRef, useEffect } from "react";
 
 import birdScene from "../assets/3d/bird.glb";
-import { useGLTF } from "@react-three/drei";
+import { useAnimations, useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 
 const Bird = () => {
-  const { scene, animations } = useGLTF(birdScene);
   const birdRef = useRef();
+  const { scene, animations } = useGLTF(birdScene);
+  const { actions } = useAnimations(animations, birdRef); //libreria para usar la animacion
+
+  useEffect(() => {
+    // se utilizan las animaciones
+    actions["Take 001"].play(); //le digo que vuele el pajarito
+  }, []);
+
+  useFrame(({clock, camera}) => {
+    //actualiza la posicion del pajaro para simular su vuelo
+    //Actuliza la posicion y del vuelo en onda seno "matematica", sine wave
+    birdRef.current.position.y = Math.sin(clock.elapsedTime) * 0.2 + 2
+    
+    //chequear si el pajaro llego a un cierto punto final relativo a la camara
+    if(birdRef.current.position.x > camera.position.x + 10) { //fuera de camara
+      //cambiar la direccion hacia atras y rotar el pajaro 180 grados
+      birdRef.current.rotation.y = Math.PI;
+    } else if(birdRef.current.position.x < camera.position.x - 10) {
+      //cambiar la direccion hacia adelante y resetear la rotacion del pajaro
+      birdRef.current.rotation.y = 0;
+    }
+
+    //actualizar la posicion x y z basada en la direccion
+    if(birdRef.current.rotation.y === 0) { //rotacion en y, z
+      //moverse hacia adelante
+      birdRef.current.position.x += 0.01;
+      birdRef.current.position.z -= 0.01;
+    } else {
+      //moverse hacia atras
+      birdRef.current.position.x -= 0.01;
+      birdRef.current.position.z += 0.01;
+    }
+
+  });
 
   return (
-    <mesh 
-      position={[-5, 2, 1]} 
-      scale={[0.003, 0.003, 0.003]} 
-      ref={birdRef}>
+    <mesh position={[-5, 2, 1]} scale={[0.003, 0.003, 0.003]} ref={birdRef}>
       <primitive object={scene} />
     </mesh>
   );
-}
+};
 
 export default Bird;
